6c8d3a9578bd616fc10b17d1dd42346687902c23
PathriVidyaPraveen <vidyapraveen.2006@gmail.com>
2025-06-11 05:07:13 +0000

    Added labelled dataset csv file and main.py for main program

---
+++ main.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import cv2
+
+from sklearn.decomposition import PCA
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split,GridSearchCV
+from sklearn.metrics import classification_report,confusion_matrix
+
